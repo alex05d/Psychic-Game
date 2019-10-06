208e4f57b4e222274df4288443d0e128f167b6f4
Alexiss Diaz <alexiss.diaz@live.com>
2019-10-05 22:38:13 -0500

    fixed bug

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -8,7 +8,7 @@ const guessText = document.getElementById("tiresT");
 
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var computerPick = options[Math.floor(Math.random() * options.length)];
-console.log(computerPick);
+
 
 var wins = 0;
 var loss = 0;
@@ -24,14 +24,12 @@ var reset = function () {
     userPicks.innerHTML = userguess;
     computerPick = options[Math.floor(Math.random() * options.length)];
 }
-console.log(reset);
+
 
 //--user picks a letter 
 document.onkeyup = function (event) {
     input = event.key;
     picks = input.toLowerCase();
-
-    console.log(picks);
 
 
     if (!userguess.includes(input)) {
@@ -46,7 +44,7 @@ document.onkeyup = function (event) {
         }
         else if (picks === picks) {
             tries--;
-            triesT.innerHTML = tires;
+            triesT.innerHTML = tries;
         }
         if (tries === 0 && picks !== computerPick) {
             loss++;
